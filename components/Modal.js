ef30093b6fe375321f8205702bbdb92dd06c911d
akrangare <akrangare@gmail.com>
2024-01-17 21:00:41 +0000

    solved 2

--- components/Modal.js
+++ components/Modal.js
@@ -4,7 +4,10 @@ const Modal = ({ children, isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
-    <div className="modal">
+    <div className="modal" onClick={()=>{onClose()}}
+
+
+    >
       <div
         style={{
           backgroundColor: "white",
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,24 @@
-import { useState } from 'react';
-import Modal from '../components/Modal';
+import { useState } from "react";
+import Modal from "../components/Modal";
 
 const HomePage = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  function onClose() {
+    console.log("onClose");
+    setIsOpen(false);
+  }
 
   return (
     <div>
-      <button onClick = {()=>setIsOpen(true)}>Open Modal</button>
+      <button onClick={() => setIsOpen(true)}>Open Modal</button>
 
-      <Modal isOpen={isOpen} >
+      <Modal isOpen={isOpen} onClose={onClose}>
         <h2>This is a Modal</h2>
         <p>Using React Portals with Next.js</p>
-        <button onClick={()=>setIsOpen(false)}>Close Modal</button>
+        <button onClick={() => setIsOpen(false)}>Close Modal</button>
       </Modal>
-    </div> 
+    </div>
   );
 };
 
